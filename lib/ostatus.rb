9b4f9827a69ac997f2b3a4c1b23827a532a69f48
wilkie <wilkie05@gmail.com>
2011-03-12 17:59:16 -0500

    Added some empty methods for our api.

--- lib/ostatus.rb
+++ lib/ostatus.rb
@@ -2,7 +2,18 @@ require 'oauth/tokens/access_token'
 
 module OStatus
   class Feed
-    def initialize(OAuth::AccessToken access_token, )
+    def initialize(access_token, url)
+      @url = url
+      @access_token = access_token
+    end
+
+    def retrieve_atom
+      @access_token.get(@url)
+    end
+
+    def entries
+      atom = retrieve_atom()
+
     end
   end
 end
