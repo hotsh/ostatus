f01b2ce84fd4e69f9ca48cfd3744566e0cd865b6
wilkie <wilkie05@gmail.com>
2011-03-12 19:46:42 -0500

    Changing the order of the parameters to make the access token optional.

--- lib/ostatus/feed.rb
+++ lib/ostatus/feed.rb
@@ -1,16 +1,21 @@
 require 'nokogiri'
+require 'open-uri'
 
 require_relative 'entry'
 
 module OStatus
   class Feed
-    def initialize(access_token, url)
+    def initialize(url, access_token = nil)
       @url = url
       @access_token = access_token
     end
 
     def retrieve_atom
-      @access_token.get(@url)
+      if @access_token == nil
+        open(@url).read
+      else
+        @access_token.get(@url).body
+      end
     end
 
     def author
@@ -18,7 +23,7 @@ module OStatus
 
     def entries
       atom = retrieve_atom()
-      xml = Nokogiri::XML::Document.parse(atom.body)
+      xml = Nokogiri::XML::Document.parse(atom)
       entries_xml = xml.css('entry')
 
       entries_xml.map do |entry|
