36907ea716e8d78469b547489ed189808912c9dc
wilkie <wilkie05@gmail.com>
2011-03-12 21:28:01 -0500

    Fixing Activity to take the parent Element and not a NodeSet and adding documentation.

--- lib/ostatus/activity.rb
+++ lib/ostatus/activity.rb
@@ -1,7 +1,12 @@
 module OStatus
+
+  # This class represents an Activity object for an OStatus entry.
   class Activity
-    def initialize(activity_nodeset)
-      @activity = activity_nodeset
+
+    # This will create an instance of an Activity class populated
+    # with the given XML data.
+    def initialize(activity_root)
+      @activity = activity_root
     end
 
     def pick_first_node(a)
@@ -13,22 +18,27 @@ module OStatus
     end
     private :pick_first_node
 
+    # Returns the object field or nil if it does not exist.
     def object
-      pick_first_node(@activity.xpath('//activity:verb'))
+      pick_first_node(@activity.xpath('./activity:object'))
     end
 
+    # Returns the target field or nil if it does not exist.
     def target
-      pick_first_node(@activity.xpath('//activity:target'))
+      pick_first_node(@activity.xpath('./activity:target'))
     end
 
+    # Returns the verb field or nil if it does not exist.
     def verb
-      pick_first_node(@activity.xpath('//activity:verb'))
+      pick_first_node(@activity.xpath('./activity:verb'))
     end
 
+    # Returns the object-type field or nil if it does not exist.
     def object_type
-      pick_first_node(@activity.xpath('//activity:object_type'))
+      pick_first_node(@activity.xpath('./activity:object-type'))
     end
 
+    # Returns a hash of all relevant fields.
     def info
       {
         :object => self.object,
