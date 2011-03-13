439ece958e00af5c1bd84c55f2df3ba50e902520
wilkie <wilkie05@gmail.com>
2011-03-12 20:03:09 -0500

    Adding documentation.

--- lib/ostatus/feed.rb
+++ lib/ostatus/feed.rb
@@ -4,12 +4,20 @@ require 'open-uri'
 require_relative 'entry'
 
 module OStatus
+
+  # This class represents an OStatus Feed object.
   class Feed
+
+    # Creates a new Feed instance given by the atom feed located at 'url'
+    # and optionally using the OAuth::AccessToken given.
     def initialize(url, access_token = nil)
       @url = url
       @access_token = access_token
     end
 
+    # This method will return a String containing the actual content of
+    # the atom feed. It will make a network request (through OAuth if
+    # an access token was given) to retrieve the document if necessary.
     def atom
       if @access_token == nil
         open(@url).read
@@ -21,6 +29,8 @@ module OStatus
     def author
     end
 
+    # This method gives you an array of OStatus::Entry instances for 
+    # each entry listed in the feed.
     def entries
       xml = Nokogiri::XML::Document.parse(self.atom)
       entries_xml = xml.css('entry')
