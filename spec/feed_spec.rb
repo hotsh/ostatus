8db1fa8959af86c33f5ac24a952faaf85b7a1aaa
wilkie <wilkie05@gmail.com>
2013-03-14 17:02:50 -0400

    Deletes a failing test without an assert.

--- spec/feed_spec.rb
+++ spec/feed_spec.rb
@@ -2,7 +2,6 @@ require_relative '../lib/ostatus/feed.rb'
 require_relative '../lib/ostatus/entry.rb'
 
 describe OStatus::Feed do
-
   describe "#initialize" do
     it "should detect a feed URI in an HTML page" do
       @feed = OStatus::Feed.from_url('test/example_page.html')
@@ -39,12 +38,6 @@ describe OStatus::Feed do
           entry.instance_of?(OStatus::Entry).should eql(true)
         end
       end
-    end
-  end
-
-  describe "#from_url" do
-    it "should be able to sort link elements with unknown MIME types" do
-      @feed = OStatus::Feed.from_url('test/mime_type_bug_feed.atom')
     end
   end
 end
