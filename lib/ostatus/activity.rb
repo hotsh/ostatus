93877fb30d3d99e8745440252980c68e64ce8a93
wilkie <wilkie05@gmail.com>
2013-03-14 22:16:21 -0400

    Cleans up and limits the role of Activity.

--- lib/ostatus/activity.rb
+++ lib/ostatus/activity.rb
@@ -1,79 +1,42 @@
 module OStatus
-  # This class represents an Activity object for an OStatus entry.
+  # This class represents an Activity object for an OStatus::Entry.
   class Activity
+    # The XML namespace that identifies the conforming specification.
     NAMESPACE = 'http://activitystrea.ms/spec/1.0/'
+
+    # The XML schema that identifies the conforming schema for objects.
     SCHEMA_ROOT = 'http://activitystrea.ms/schema/1.0/'
 
-    def initialize(entry)
+    # The object of this activity.
+    attr_reader :object
+
+    # The type of object for this activity.
+    #
+    # The field can be a String for uncommon types. Several are standard:
+    #   :article, :audio, :bookmark, :comment, :file, :folder, :group,
+    #   :list, :note, :person, :photo, :"photo-album", :place, :playlist,
+    #   :product, :review, :service, :status, :video
+    attr_reader :object_type
+
+    # The action being invoked in this activity.
+    #
+    # The field can be a String for uncommon verbs. Several are standard:
+    #   :favorite, :follow, :like, :"make-friend", :join, :play,
+    #   :post, :save, :share, :tag, :update
+    attr_reader :verb
+
+    # The target of the action.
+    attr_reader :target
+
+    # Creates a new representation of an activity.
+    #
+    # options:
+    #   :object      => The object of this activity.
+    #   :object_type => The type of object for this activity.
+    #   :target      => The target of this activity.
+    #   :verb        => The action of the activity.
+    def initialize(options = {})
       @entry = entry
-    end
-
-    # Returns the object field or nil if it does not exist.
-    def object
-      if @entry.is_a? Hash
-        @entry[:object]
-      else
-        @entry.activity_object
-      end
-    end
-
-    # Returns the target field or nil if it does not exist.
-    def target
-      if @entry.is_a? Hash
-        @entry[:object]
-      else
-        @entry.activity_target
-      end
-    end
-
-    # Returns the verb field or nil if it does not exist.
-    # :favorite, :follow, :like, :"make-friend", :join, :play,
-    # :post, :save, :share, :tag, :update
-    def verb
-      if @entry.is_a? Hash
-        @entry[:object]
-      else
-        obj = @entry.activity_verb
-        if obj.nil?
-          nil
-        elsif obj.start_with?(SCHEMA_ROOT)
-          obj[SCHEMA_ROOT.size..-1].intern unless obj.nil?
-        else
-          obj
-        end
-      end
-    end
-
-    def verb= value
-      if @entry.is_a? Hash
-        @entry[:object] = value
-      else
-        if [:favorite, :follow, :like, :"make-friend", :join,
-          :play, :save, :share, :tag, :update].include? value
-          value = "#{SCHEMA_ROOT}#{value}"
-        end
-
-        @entry.activity_verb = value
-      end
-    end
-
-    # Returns the object-type field or nil if it does not exist.
-    # :article, :audio, :bookmark, :comment, :file, :folder, :group,
-    # :list, :note, :person, :photo, :"photo-album", :place, :playlist,
-    # :product, :review, :service, :status, :video
-    def object_type
-      if @entry.is_a? Hash
-        @entry[:object_type]
-      else
-        obj = @entry.activity_object_type
-        if obj.nil?
-          nil
-        elsif obj.start_with?(SCHEMA_ROOT)
-          obj[SCHEMA_ROOT.size..-1].intern unless obj.nil?
-        else
-          obj
-        end
-      end
     end
 
     # Returns a hash of all relevant fields.
