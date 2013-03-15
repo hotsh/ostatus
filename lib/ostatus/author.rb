099105aad817222c064d8dd8b56d3c4b6b04acc5
wilkie <wilkie05@gmail.com>
2013-03-14 21:12:14 -0400

    Cleans up and limits the role of OStatus::Author.

--- lib/ostatus/author.rb
+++ lib/ostatus/author.rb
@@ -5,89 +5,34 @@ module OStatus
   require 'atom'
 
   # Holds information about the author of the Feed.
-  class Author < ::Atom::Person
+  class Author
     require 'date'
 
-    include ::Atom::SimpleExtensions
+    # The uri that uniquely identifies the author.
+    attr_reader :uri
 
-    add_extension_namespace :activity, OStatus::Activity::NAMESPACE
-    element 'activity:object-type'
+    # The email address of the author.
+    attr_reader :email
 
-    namespace ::Atom::NAMESPACE
-    element :email
-    element :uri
+    # The OStatus::PortableContacts that represents the author.
+    attr_reader :portable_contacts
 
-    elements :links, :class => ::Atom::Link
+    # The name of the author
+    attr_reader :name
 
-    add_extension_namespace :poco, POCO_NS
-    element 'poco:id'
-    element 'poco:displayName'
-    element 'poco:nickname'
-    element 'poco:updated',     :class => DateTime, :content_only => true
-    element 'poco:published',   :class => DateTime, :content_only => true
-    element 'poco:birthday',    :class => Date, :content_only => true
-    element 'poco:anniversary', :class => Date, :content_only => true
-    element 'poco:gender'
-    element 'poco:note'
-    element 'poco:preferredUsername'
-    element 'poco:connected'
-
-    def initialize *args
-      self.activity_object_type = "http://activitystrea.ms/schema/1.0/person"
-      super(*args)
-    end
-
-    # unfortunately ratom doesn't handle elements with the same local name well.
-    # this is a workaround for that.
-    attr_writer :name, :poco_name
-
-    def name
-      @name or self[::Atom::NAMESPACE, 'name'].first
-    end
-
-    def poco_name
-      @poco_name or self[POCO_NS, 'name'].first
-    end
-
-    def to_xml(*args)
-      x = super(*args)
-
-      if self.name
-        node = XML::Node.new('name')
-        node << self.name
-        x << node
-      end
-
-      if self.poco_name
-        node = XML::Node.new('poco:name')
-        node << self.poco_name
-        x << node
-      end
-
-      x
-    end
-
-    # Gives an instance of an OStatus::Activity that parses the fields
-    # having an activity prefix.
-    def activity
-      OStatus::Activity.new(self)
-    end
-
-    # Returns an instance of a PortableContacts that further describe the
-    # author's contact information, if it exists.
-    def portable_contacts
-      PortableContacts.new(self)
-    end
-
-    def portable_contacts= poco
-      [ 'id', 'name', 'nickname', 'updated', 'published', 'birthday',
-        'anniversary', 'gender', 'note', 'connected'].each do |p|
-        v = poco.send(p)
-        self.send("poco_#{p}=", v) if v
-      end
-
-      self.poco_displayName = poco.display_name if poco.display_name
-      self.poco_preferredUsername = poco.preferred_username if poco.preferred_username
+    # Creates a representating of an author.
+    #
+    # options:
+    #   uri               => The uri that uniquely identifies this author.
+    #   name              => The name of the author. Defaults: "anonymous"
+    #   email             => The email of the author.
+    #   portable_contacts => The OStatus::PortableContacts representing this
+    #                        author.
+    def initialize(options = {})
+      @uri = options[:uri]
+      @name = options[:name] || "anonymous"
+      @email = options[:email]
+      @portable_contacts = options[:portable_contacts]
     end
   end
 end
