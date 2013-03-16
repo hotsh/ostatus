be6a2a3b105fb02010119c1b7978568b0b44ff83
wilkie <wilkie05@gmail.com>
2013-03-16 16:28:09 -0400

    Tests Author.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,60 +2,22 @@ require_relative 'helper'
 require_relative '../lib/ostatus/author.rb'
 
 describe OStatus::Author do
-  before(:each) do
-    feed = OStatus::Feed.from_url('test/example_feed.atom')
-    @author = feed.author
-    feed = OStatus::Feed.from_url('test/example_feed_empty_author.atom')
-    @author_empty = feed.author
-  end
-
-  describe "#activity" do
-    it "should return an Activity instance" do
-      @author.activity.class.must_equal(OStatus::Activity)
+  describe "#initialize" do
+    it "should store an uri" do
+      OStatus::Author.new(:uri => "http://example.com/1").uri.must_equal "http://example.com/1"
     end
 
-    it "should give an Activity instance that is relevant to the author subtree" do
-      @author.activity.object_type.must_equal(:person)
-    end
-  end
-
-  describe "#portable_contacts" do
-    it "should return an PortableContacts instance" do
-      @author.portable_contacts.class.must_equal(OStatus::PortableContacts)
+    it "should store a name" do
+      OStatus::Author.new(:name => "foo").name.must_equal "foo"
     end
 
-    it "should give an PortableContacts instance that is relevant to the author subtree" do
-      @author.portable_contacts.preferred_username.must_equal('greenmanspirit')
-    end
-  end
-
-  describe "#uri" do
-    it "should give a String containing the content of the uri tag" do
-      @author.uri.must_equal('http://identi.ca/user/141464')
+    it "should store a email" do
+      OStatus::Author.new(:email => "foo@example.com").email.must_equal "foo@example.com"
     end
 
-    it "should give nil when no uri is given" do
-      @author_empty.uri.must_equal(nil)
-    end
-  end
-
-  describe "#name" do
-    it "should give a String containing the content of the name tag" do
-      @author.name.must_equal('greenmanspirit')
-    end
-
-    it "should give nil when no name is given" do
-      @author_empty.name.must_equal(nil)
-    end
-  end
-
-  describe "email" do
-    it "should give a String containing the content of the email tag" do
-      @author.email.must_equal('foo@example.com')
-    end
-
-    it "should give nil when no email is given" do
-      @author_empty.email.must_equal(nil)
+    it "should store a PortableContacts" do
+      poco = mock('portable_contacts')
+      OStatus::Author.new(:portable_contacts => poco).portable_contacts.must_equal poco
     end
   end
 end
