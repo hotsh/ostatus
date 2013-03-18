fcbc6ca5a534c62433b92803376f5a9807a9aad3
wilkie <wilkie05@gmail.com>
2013-03-18 05:36:56 -0400

    Adds Atom generation <author> and some <poco:*> tests.

--- spec/atom/feed_spec.rb
+++ spec/atom/feed_spec.rb
@@ -232,6 +232,168 @@ describe OStatus::Atom do
           @subtitle.attributes.get_attribute('type').value.must_equal @master.subtitle_type
         end
       end
+
+      describe "<author>" do
+        before do
+          @author = @feed.find_first('xmlns:author', 'xmlns:http://www.w3.org/2005/Atom')
+        end
+
+        describe "<activity:object-type>" do
+          it "should identify this tag as a person object" do
+            @author.find_first('activity:object-type').content
+              .must_equal "http://activitystrea.ms/schema/1.0/person"
+          end
+        end
+
+        describe "<email>" do
+          it "should list the author's email" do
+            @author.find_first('xmlns:email',
+                               'xmlns:http://www.w3.org/2005/Atom').content.must_equal @master.authors.first.email
+          end
+        end
+
+        describe "<uri>" do
+          it "should list the author's uri" do
+            @author.find_first('xmlns:uri',
+                               'xmlns:http://www.w3.org/2005/Atom').content.must_equal @master.authors.first.uri
+          end
+        end
+
+        describe "<poco:id>" do
+          it "should list the author's portable contact id" do
+            @author.find_first('poco:id',
+                               'http://portablecontacts.net/spec/1.0').content.must_equal @master.authors.first.portable_contacts.id
+          end
+        end
+
+        describe "<poco:name>" do
+          before do
+            @poco_name = @author.find_first('poco:name',
+                                            'http://portablecontacts.net/spec/1.0')
+          end
+
+          describe "<formatted>" do
+            it "should list the author's portable contact formatted name" do
+              @poco_name.find_first('xmlns:formatted',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:formatted]
+            end
+          end
+
+          describe "<familyName>" do
+            it "should list the author's portable contact family name" do
+              @poco_name.find_first('xmlns:familyName',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:family_name]
+            end
+          end
+
+          describe "<givenName>" do
+            it "should list the author's portable contact given name" do
+              @poco_name.find_first('xmlns:givenName',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:given_name]
+            end
+          end
+
+          describe "<middleName>" do
+            it "should list the author's portable contact middle name" do
+              @poco_name.find_first('xmlns:middleName',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:middle_name]
+            end
+          end
+
+          describe "<honorificPrefix>" do
+            it "should list the author's portable contact honorific prefix" do
+              @poco_name.find_first('xmlns:honorificPrefix',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:honorific_prefix]
+            end
+          end
+
+          describe "<honorificSuffix>" do
+            it "should list the author's portable contact honorific suffix" do
+              @poco_name.find_first('xmlns:honorificSuffix',
+                                    'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.name[:honorific_suffix]
+            end
+          end
+        end
+
+        describe "<poco:organization>" do
+          before do
+            @poco_org = @author.find_first('poco:organization',
+                                           'http://portablecontacts.net/spec/1.0')
+          end
+
+          describe "<name>" do
+            it "should list the author's portable contact organization name" do
+              @poco_org.find_first('xmlns:name',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:name]
+            end
+          end
+
+          describe "<department>" do
+            it "should list the author's portable contact organization department" do
+              @poco_org.find_first('xmlns:department',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:department]
+            end
+          end
+
+          describe "<title>" do
+            it "should list the author's portable contact organization title" do
+              @poco_org.find_first('xmlns:title',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:title]
+            end
+          end
+
+          describe "<type>" do
+            it "should list the author's portable contact organization type" do
+              @poco_org.find_first('xmlns:type',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:type]
+            end
+          end
+
+          describe "<startDate>" do
+            it "should list the author's portable contact organization startDate" do
+              time = @poco_org.find_first('xmlns:startDate',
+                                          'xmlns:http://www.w3.org/2005/Atom').content
+              DateTime::parse(time).to_s
+                .must_equal @master.authors.first.portable_contacts.organization[:start_date].to_datetime.to_s
+            end
+          end
+
+          describe "<endDate>" do
+            it "should list the author's portable contact organization endDate" do
+              time = @poco_org.find_first('xmlns:endDate',
+                                          'xmlns:http://www.w3.org/2005/Atom').content
+              DateTime::parse(time).to_s
+                .must_equal @master.authors.first.portable_contacts.organization[:end_date].to_datetime.to_s
+            end
+          end
+
+          describe "<location>" do
+            it "should list the author's portable contact organization location" do
+              @poco_org.find_first('xmlns:location',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:location]
+            end
+          end
+
+          describe "<description>" do
+            it "should list the author's portable contact organization description" do
+              @poco_org.find_first('xmlns:description',
+                                   'xmlns:http://www.w3.org/2005/Atom')
+                .content.must_equal @master.authors.first.portable_contacts.organization[:description]
+            end
+          end
+        end
+      end
     end
   end
 end
